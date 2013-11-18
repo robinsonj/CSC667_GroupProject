16f9b374a4b6126bbbf8cf22cde2cc08e5c4e91a
Marco <redyoonnk1@yahoo.com>
2013-11-18 01:03:49 -0800

    homepage kinda done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require twitter/bootstrap
+
+$(document).ready(function() {
+	$('#content').load('pages/front'); //loads when user first visits homepage
+});
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def front
+  end
+  def events
+  end
+end
