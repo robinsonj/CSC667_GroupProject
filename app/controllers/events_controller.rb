8398aeb5e743553ebaa310985e7f7992d318764e
Marco <redyoonnk1@yahoo.com>
2013-12-11 03:44:16 -0800

    finished everything! Just need search

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-	before_filter :authenticate_user!, :except => [:eventslist]
+	before_filter :authenticate_user!, :except => [:eventslist, :show]
   def new
   	#look in application_controller.rb for set date variables
   end
@@ -17,7 +17,7 @@ class EventsController < ApplicationController
   def create
   	if params[:title].present? && params[:message].present?
 	      Event.create(:title => params[:title], :message => params[:message], :month => params[:month],
-	      	:day => params[:day], :dayofweek=> params[:dayofweek], :photo1=> params[:photo1], :user_id => current_user.id)
+	      	:day => params[:day], :dayofweek=> params[:dayofweek], :photo1=> params[:photo1], :user_id => params[:userid])
 	      render "/pages/confirmation"
 	      return #if it's a creation call, exit after.
 	    else
